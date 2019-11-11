d2d1f0b18ab1c32e48d3e7c622acecb74d207948
Yuanhao Zhang <cercle@bu.edu>
2019-11-11 11:56:25 -0500

    switch

---
+++ bash.js
@@ -0,0 +1,10 @@
+process.stdout.write("prompt>");
+process.stdin.on("data", data => {
+  const cmd = data.toString().trim();
+  process.stdout.write("You typed: " + cmd);
+  process.stdout.write("\nprompt> ");
+  if (cmd === "pwd") {
+    // console.log(process.cwd());
+    process.stdout.write(process.cwd());
+  }
+});
